e0af47f9e8ba0073b7838203f2d68988713e5f91
Samuel Parke <samueljparke@gmail.com>
2025-07-25 07:43:29 +0100

    mint and burn role instead of only owner. this will allow the saving account to mint tokens to user who deposits eth

--- src/WinToken.sol
+++ src/WinToken.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.24;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract WinToken is ERC20, Ownable, ERC20Burnable {
+contract WinToken is ERC20, AccessControl, Ownable, ERC20Burnable {
     error WinToken__MustBeMoreThanZero();
     error WinToken__BalanceMustExceedBurnAmount();
+
+    bytes32 private constant MINT_AND_BURN_ROLE = keccak256("MINT_AND_BURN_ROLE");
 
     modifier moreThanZero(uint256 _amount) {
         if (_amount == 0) {
@@ -18,12 +21,21 @@ contract WinToken is ERC20, Ownable, ERC20Burnable {
 
     constructor() ERC20("WinToken", "WIN") Ownable(msg.sender) {}
 
-    function mint(address _user, uint256 _amount) public moreThanZero(_amount) returns (bool) {
+    function grantMintAndBurnRole(address _user) external onlyOwner {
+        _grantRole(MINT_AND_BURN_ROLE, _user);
+    }
+
+    function mint(address _user, uint256 _amount)
+        public
+        moreThanZero(_amount)
+        onlyRole(MINT_AND_BURN_ROLE)
+        returns (bool)
+    {
         _mint(_user, _amount);
         return true;
     }
 
-    function burn(uint256 _amount) public override moreThanZero(_amount) onlyOwner {
+    function burn(uint256 _amount) public override moreThanZero(_amount) onlyRole(MINT_AND_BURN_ROLE) {
         uint256 userBalance = balanceOf(msg.sender);
         if (userBalance < _amount) {
             revert WinToken__BalanceMustExceedBurnAmount();
